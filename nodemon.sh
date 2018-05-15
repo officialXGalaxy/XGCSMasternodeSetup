8a8455ae93767915db0e6f1a1fb221428dd933b6
Dwigt007 <37881405+Dwigt007@users.noreply.github.com>
2018-05-15 12:06:52 -0500

    Update nodemon.sh

--- nodemon.sh
+++ nodemon.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-# nodemon 1.0 - Itis Masternode Monitoring 
+# nodemon 1.0 - Reef Masternode Monitoring 
 
 #Processing command line params
 if [ -z $1 ]; then dly=1; else dly=$1; fi   # Default refresh time is 1 sec
 
-datadir="/$USER/.itis$2"   # Default datadir is /root/.itis
+datadir="/$USER/.reefcore$2"   # Default datadir is /root/.reefcore
  
 # Install jq if it's not present
 dpkg -s jq 2>/dev/null >/dev/null || sudo apt-get -y install jq
 
 #It is a one-liner script for now
 watch -ptn $dly "echo '===========================================================================
-Outbound connections to other Itis.Network nodes [itis datadir: $datadir]
+Outbound connections to other Reef nodes [itis datadir: $datadir]
 ===========================================================================
 Node IP               Ping    Rx/Tx     Since  Hdrs   Height  Time   Ban
 Address               (ms)   (KBytes)   Block  Syncd  Blocks  (min)  Score
 ==========================================================================='
-itis-cli -datadir=$datadir getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor) ,\
+./reef-cli -datadir=$datadir getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor) ,\
 \(.bytesrecv/1024|floor)/\(.bytessent/1024|floor),\(.startingheight) ,\(.synced_headers) ,\(.synced_blocks)  ,\
 \((now-.conntime)/60|floor) ,\(.banscore)\"' | column -t -s ',' && 
 echo '==========================================================================='
 uptime
 echo '==========================================================================='
-echo 'Masternode Status: \n# itis-cli masternode status' && itis-cli -datadir=$datadir masternode status
+echo 'Masternode Status: \n# ./reef-cli masternode status' && ./reef-cli -datadir=$datadir masternode status
 echo '==========================================================================='
-echo 'Sync Status: \n# itis-cli mnsync status' &&  itis-cli -datadir=$datadir mnsync status
+echo 'Sync Status: \n# ./reef-cli mnsync status' &&  ./reef-cli -datadir=$datadir mnsync status
 echo '==========================================================================='
-echo 'Masternode Information: \n# itis-cli getinfo' && itis-cli -datadir=$datadir getinfo
+echo 'Masternode Information: \n# ./reef-cli getinfo' && ./reef-cli -datadir=$datadir getinfo
 echo '==========================================================================='
 echo 'Usage: nodemon.sh [refresh delay] [datadir index]'
-echo 'Example: nodemon.sh 10 22 will run every 10 seconds and query itisd in /$USER/.itis22'
+echo 'Example: nodemon.sh 10 22 will run every 10 seconds and query reefd in /$USER/.reefcore22'
 echo '\n\nPress Ctrl-C to Exit...'"
